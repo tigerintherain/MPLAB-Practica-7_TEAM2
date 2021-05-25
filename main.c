1c3ddd1f933da35ffe8e05370e7b322005847aae
tigerintherain <79423145+tigerintherain@users.noreply.github.com>
2021-05-25 09:56:06 -0500

    Update main.c

--- main.c
+++ main.c
@@ -12,6 +12,8 @@
 #define LCD_E               LATCbits.LATC0
 #define LCD_E_DIR           TRISCbits.TRISC0
 
+#define PULSE               PORT.RA4
+
 #define _XTAL_FREQ 8000000
 #define SWEEP_FREQ 200
 
@@ -23,33 +25,94 @@ void LCD_init(void);
 void LCD_cmd(char cx);
 void send2LCD(char xy);
 
-void main(void){
+int overflow; //for interrupts
+char cnt;
+
+char LB, HB; //for frequency calculation
+long t_elapse;
+
+void __interrupt (high_priority)  high_priority_ISR(void)
+{
+ if (INTCON.TMROIF == 1)  // if TIMER0 interrupt occurs
+{
+     overflow++;          // increments overflow count
+     INTCON.TMR0IF = 0;   //clears intrrupt flag
+}
+ if (PIR1.TM1IF == 1 )    // if TIMER1 interrupt occurs
+{
+     TMR1H = 0x0B;       //Time register low and high
+     TMR1L = 0xDC;       
+     cnt++;              //Increments count (cnt)
+     PIR1.TMR1IF = 0;     // Clear Timer1 interrrupt flag
+}
+}
+
+void main(void)
+{
+portsInit();
+LCD_init();
+ 
 OSCCON = 0b01110110;// Set the internal oscillator to 8MHz and stable
 LCD_DATA_DIR = 0x00;
 LCD_RS = 0;
 LCD_RW = 0;
 LCD_E  = 0;
-
-void LCD_cmd(0x80);//set cursor to first line
-void sen2LCD("frequency:");
  
-portsInit();
-LCD_init();
+LCD_cmd(0x80);//set cursor to first line
+send2LCD("frequency:");
 
-while (1){
-    
+T0CON = 0x28; //TIMER0 as 16-bit, no prescaler
+T1CON = 0x36; //TIMER1 for 250 ms overflow
  
-void LCD_cmd(0xC0); //set cursos to second line
-void send2LCD("Hertz");
+PIE1 = 0x01  //Enable TIMER 1 interrupts
+PIR1.TMR1IF = 0; //Clear TIMER1 interrupt flag
+INTCON = 0xE0;   //Enable TIMER0 AND TIMER1 interrupts
+
+while (1)
+{
+TMR0H = 0; //Clear TIMER0 register
+TMR0L = 0;
+TMR1H = 0x0B; //TIMER1 registers 3036 = 0x0BDC for 250 ms
+TMR1L = 0xDC;
+overflow = 0; //clear overflow count
+cnt = 0;      //clear cnt
+ 
+TOCON.TMR0ON = 1; //start TIMER0, incrments when pulse is detected. 
+T1CON.TMR1ON = 1;//start TIMER1 to count 250ms(4) = 1 s
+
+while(cnt !=4); //wait fo 4 cycles
+ 
+TOCON.TMR0ON = 0; // Stop TIMER0
+T1CON.TMR1ON = 0; // Stop TIMER1
+
+LB = TMR0L; //gets TIMER0 values
+HB = TMROH
+ 
+t_elapse = (long)256*HB + LB;
+t_elapse = 65536*overflow + t_lapse;
+ 
+char t_elapsechar = (char)t_elapse;
+ 
+ LCD_cmd(0xC0); //set cursor to second line
+ send2LCD(t_elapsechar); //send frequency
+ 
+ LCD_cmd(0x14); //shift cursor position to right
+ LCD_cmd(0x14); //shift cursor position to right
+ LCD_cmd(0x14); //shift cursor position to right
+ LCD_cmd(0x14); //shift cursor position to right
+ 
+ send2LCD("Hz"); //send Hz
+
 }
 }
 
 void portsInit (void){
     ANSELC = digital;   // Set port C as Digital
-    TRISC = 0X00;       //RC0 to E, RC1 to R/W, RC2 to RS
+    TRISC = 0X00;       // RC0 to E, RC1 to R/W, RC2 to RS
     ANSELD = digital;   // Set port D as Digital
-    TRISD = 0x00;       // Data bus (D0-D7)
-    
+    TRISD = 0x00;       // Data bus (D0-D7) 
+    ANSELA = analog;    //set port a as analog
+    TRIS.RA4 = 1;       // Set RA4 as input
     
 }
 
